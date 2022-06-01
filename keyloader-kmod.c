77931379c3fd014df5acb3c668245d1667277078
Chris Procter <cprocter@redhat.com>
2022-06-01 09:30:56 +0100

    return error (not panic) on invalid key being passed

--- keyloader-kmod.c
+++ keyloader-kmod.c
@@ -20,9 +20,10 @@ module_param(keyid, uint, 0660);
 module_param(platformid, uint, 0660);
 
 // TODO
-// check if key already exists in .platform
-// remove key from user keyring?
-// handle add multiple keys
+// error checking, lots and lots of error checking
+// check if key already exists in .platform and if so dont remove it on exit
+// remove key from user keyring? (currently they get left there)
+// handle adding multiple keys
 
 
 struct key * get_key_from_id(uint id){
@@ -31,8 +32,8 @@ struct key * get_key_from_id(uint id){
 
     key_ref = lookup_user_key(id, 0, KEY_NEED_VIEW);
     if (IS_ERR(key_ref)) {
-        printk("ERROR to lookup_user_key %d\n", IS_ERR(key_ref));
-	return 0;
+        printk("ERROR in lookup_user_key for key: %d  error: %ld\n", id, PTR_ERR(key_ref));
+	return (struct key *) key_ref;
     }	    
     keyring = key_ref_to_ptr(key_ref);
 
@@ -44,11 +45,21 @@ int init_module(void)
 {
     int rc;
 
-    struct key* to_keyring;
-    struct key *newkey;
+    struct key * to_keyring;
+    struct key * newkey;
+
 
     to_keyring = get_key_from_id(platformid);
+    if( IS_ERR(to_keyring)){
+        printk("ERROR error resolving platformid: %d  error: %ld\n", platformid, PTR_ERR(to_keyring));
+        return PTR_ERR(to_keyring);
+    }
+
     newkey = get_key_from_id(keyid);
+    if( IS_ERR(newkey)){
+        printk("ERROR resolving keyid: %d  error: %ld\n", keyid, PTR_ERR(newkey));
+	return PTR_ERR(newkey);
+    }
 
     rc = key_link(to_keyring, newkey);
     //rc = key_move(newkey, from_keyring, to_keyring, 0);
